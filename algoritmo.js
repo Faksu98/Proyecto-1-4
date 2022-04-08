16b7847dc2defa9497d561d51ea3412d82d7b96e
faksu <dk>
2022-04-08 13:29:11 -0300

    porytecto 1-4

---
+++ algoritmo.js
@@ -0,0 +1,11 @@
+document.write(`
+<details>
+<summary>Detalles elden ring</summary>
+<p>
+    punto de gracia
+</p>
+<img width="400" height="300" src="https://abctech.news/wp-content/uploads/2022/02/site-of-grace-uses-locations-53RhA7-2.jpg" alt="punto de gracia">
+<br>
+<a href="https://eldenring.wiki.fextralife.com/Interactive+Map">donde encontralas</a>
+</details>
+`)
